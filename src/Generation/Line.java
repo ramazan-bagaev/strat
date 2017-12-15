9a525b47d28221dcc4661cd30afda4768422f79d
ramazan <ramazan.bagaev@phystech.edu>
2017-12-15 15:16:32 +0300

    something

--- src/Generation/Line.java
+++ src/Generation/Line.java
@@ -38,24 +38,27 @@ public class Line {
     }
 
     public boolean intersect(Line line){
-        int deter = alpha * line.beta - beta * line.alpha;
+        int deter = alpha * line.beta - line.alpha * beta;
         if (deter == 0){
             if (a.equals(line.a) || a.equals(line.b) || b.equals(line.a) || b.equals(line.b)) return true;
             return false;
         }
-        float t = (float)(line.beta * (line.a.x - a.x) - beta * (line.a.y - a.y)) / (float)deter;
-        float t1 = (float)(alpha * (line.a.x - a.x) - line.beta * (line.a.y - a.y))/ (float)deter;
+
+        float t1 = (float)(alpha * (line.a.y - a.y) + beta * (line.a.x - a.x))/ (float)(deter);
+        float t = (float)((line.a.x - a.x) + line.alpha * t1)/(float)alpha;
+        //float t = (float)(beta * (line.a.x - a.x) + line.beta * (line.a.y - a.y)) / (float)(deter);
+        //float t1 = (float)(line.alpha * (line.a.x - a.x) + alpha * (line.a.y - a.y))/ (float)(deter);
         System.out.println(t + " " + t1);
         float x = a.x + (b.x - a.x) * t;
-        float y = a.y + (b.y - a.x) * t;
+        float y = a.y + (b.y - a.y) * t;
         System.out.println("x = " + x  + "; y = " + y);
-        if (t > 0 && t < 1 && t1 > 0 && t1 < 1) return true;
+        if (t >= 0 && t <= 1 && t1 >= 0 && t1 <= 1) return true;
         return false;
     }
 
     public static void main(String[] argv){
-        Line line1 = new Line(new Coord(1, 1), new Coord(-1, -1));
-        Line line2 = new Line(new Coord(-2, 0), new Coord(2, 0));
+        Line line1 = new Line(new Coord(2, 7), new Coord(3, 5));
+        Line line2 = new Line(new Coord(1, 1), new Coord(3, 5));
         System.out.print(line1.intersect(line2));
     }
 }
